43891f09937bdca7e49c613f838cdf9c3cf278e4
Monica Hanson <wolfmurdurer23@gmail.com>
2023-01-18 21:55:11 -0600

    Comments and remove some debugging lines

--- main.py
+++ main.py
@@ -6,16 +6,18 @@ import os
 import sys
 import datetime
 
-class Bot(discord.Client):
+class Audit(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
     
+    # Log when a message was sent
     async def on_message(self, message):
         print(f'Message from {message.author}@{message.channel}: {message.content}')
 
         filename = f'{message.guild}.json'
         write_out = {}
 
+        # Build the dict for the message
         logged_message = {
             "sender":   str(message.author),
             "channel":  str(message.channel),
@@ -24,55 +26,62 @@ class Bot(discord.Client):
             "contents": str(message.content)
         }
 
+        # Check if the file exists and, if it does, work off of that.
         if os.path.exists(filename):
             with open(filename) as filp:
                 write_out = json.load(filp)
 
+        # Append the message to the log, then write it out.
         write_out[str(message.id)] = logged_message
-        print(write_out)
-
         with open(filename, 'w') as filp:
             filp.write(json.dumps(write_out))
     
+    # Log when a message was edited.
     async def on_message_edit(self, before, after):
         print(f'Message from {before.author}@{before.channel} (ID {before.id}' +\
                     f' now says {after.content}')
         
         filename = f'{before.guild}.json'
         write_out = {}
+
+        # Build format for edited message
         edited_message = {
             "edit_date":        after.edited_at.strftime("%Y-%m-%d_%H:%M:%S"),
             "new_body":         str(after.content)
         }
 
+        # Check if there is an existing log to work off of.
         if os.path.exists(filename):
             with open(filename) as filp:
                 write_out = json.load(filp)
 
         edit_count = write_out[str(before.id)]['edits'] + 1
 
+        # Adjust the log appropriately for the edited message, then write out
         write_out[str(before.id)]['edits']              = edit_count
         write_out[str(before.id)][f'edit_{edit_count}'] = edited_message
-        print(write_out)
-
         with open(filename, 'w') as filp:
             filp.write(json.dumps(write_out))
     
+    # Log when a message was deleted
+    # NOTE: Due to limitations of discord.py (might be the Discord API as a whole), the bot is unable to log who deleted a message
     async def on_message_delete(self, message):
-        print(f'Message from {message.author}@{message.channel} (ID {message.id}' +\
+        print(f'Message from {message.author}@{message.channel} (ID {message.id})' +\
                     f' was deleted')
         
         filename = f'{message.guild}.json'
         write_out = {}
+
+        # Homebrew method for checking when a message was deleted. May not be entirely accurate.
         deleted_time = datetime.datetime.now()
 
+        # Check if there is an existing log to work off of.
         if os.path.exists(filename):
             with open(filename) as filp:
                 write_out = json.load(filp)
 
+        # Modify log to note when a message was deleted, then write out.
         write_out[str(message.id)]['deleted_time'] = deleted_time.strftime("%Y-%m-%d_%H:%M:%S")
-        print(write_out)
-
         with open(filename, 'w') as filp:
             filp.write(json.dumps(write_out))
 
@@ -111,8 +120,8 @@ def main():
     intents = discord.Intents.default()
     intents.message_content = True
 
-    bot = Bot(intents=intents)
-    bot.run(token)
+    audit = Audit(intents=intents)
+    audit.run(token)
 
 if __name__ == '__main__':
     main()
